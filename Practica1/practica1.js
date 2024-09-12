aabf5657ccdd543b6c2e9c2e62f462d3bcaf5151
Melissa Ruiz <melruizglez97@gmail.com>
2024-09-12 20:25:16 +0100

    Se corrigen funciones, falta establecer correctamente el borde de la tabla

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -6,8 +6,17 @@
     let max = 10;
     let num = 2;
     let contain = "";
+    let elFila = document.createElement("tr");
+    let elCol = document.createElement("td");
+    elFila.appendChild(elCol);
+    
     for (let i = 0; i <= max; i++) {
+        
         contain+= num + " * "+i+ " = " +(i*num) + "\n";
+        elCol.innerHTML = contain;
+        elFila.appendChild(elCol);
+        DOM.table.appendChild(elFila);
+        
     }
     DOM.table.innerHTML = contain;
 
