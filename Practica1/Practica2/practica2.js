ee0d9de14265a3d3cf3c3ad8f87e32aa144295c1
Melissa Ruiz <melruizglez97@gmail.com>
2024-09-16 19:59:57 +0100

    refactor: se corrige practica

---
+++ Practica1/Practica2/practica2.js
@@ -0,0 +1,19 @@
+const DOM = {
+    table : document.getElementById("tabla"),
+}
+
+function generateTable() {
+let 
+
+for (let i = 0; i <= max; i++) {
+    
+    contain+= num + " * "+i+ " = " +(i*num) + "\n";
+    elCol.innerHTML = contain;
+    
+    
+}
+DOM.table.innerHTML = contain;
+
+}
+
+generateTable()
