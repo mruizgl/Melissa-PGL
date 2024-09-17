e9ee0ca93aeeaf71d6c22eff3c3172f41375e653
Melissa Ruiz <melruizglez97@gmail.com>
2024-09-17 20:09:53 +0100

    chore[juego-memoria]: se añaden comentarios explicativos para comprender la algoritmia del script

---
+++ Practica1/Juego/script.js
@@ -0,0 +1,17 @@
+/*
+(int) (casillas / 2)
+
+set aleatorios
+
+while (aleatorios.size < (int) (casillas / 2 ) ) {
+    int random = (int) (Math.random() * casillas);
+    if (!aleatorios.contains(random)) {
+        aleatorios.add(random);
+    }
+}
+
+while array.size < cuantia {
+    rnd = aleatoriogenerado()
+}
+
+*/
