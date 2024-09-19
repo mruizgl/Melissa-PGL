d8266c56861f0aede7432a968bf8c0e1c21424d4
Melissa Ruiz <melruizglez97@gmail.com>
2024-09-19 20:24:56 +0100

    feat[ahorcado]: se añade canva

--- UT1/ejemplo/prueba.js
+++ UT1/ejemplo/prueba.js
@@ -26,3 +26,31 @@ DOM.inputApuesta.addEventListener(
         }
     }
 )
+
+function canvasTest () {
+    let ctx = DOM.canvas.getContext('2d');
+
+    //limpiar el espacio de dibujo
+    ctx.clearRect(0,0,DOM.canvas.width, DOM.canvas.height);
+
+    //Dibujando lineas
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 6;
+
+    ctx.moveTo(75,200);
+    ctx.lineTo(125, 200);
+    ctx.moveTo(100,200);
+    ctx.lineTo( 100, 50);
+
+    ctx.stroke();
+
+
+    //Dibujar cabeza:
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.ellipse(203,83,8,11,0,0, Math.PI*2); //se establece centro y radios
+            
+    ctx.stroke();
+
+}
