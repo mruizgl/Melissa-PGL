88730c36c932b4ad93e422020866d1a26545681a
Melissa Ruiz <melruizglez97@gmail.com>
2024-09-26 16:21:34 +0100

    feat[dossier-node]: se comienza practica 8

---
+++ UT1/dossier-node/practica8/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const url = require('url');
+
+const puerto = 8000;
+http.createServer((req, res) => {
+    res.write(`servidor funcionando en ${puerto}`);
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+    let texto = "";
+    Object.entries(queryObject)
+        .forEach( (par) => texto += (par[0] + ": "+ par[1]));
+        res.write(`se escribio: ${texto}`);
+        res.end();
+
+}).listen(puerto, () => console.log(`Servidor escuchando en el puerto ${puerto}`));
